79a46f864489125fcca19449cb1a68a6b35bd333
KirillGaydamaka <k.gaydamaka@gmail.com>
2019-10-01 11:59:47 +0300

    Решение с применением json.dumps

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
 # Prettify JSON
 
-[TODO. There will be project description]
+Скрипт для отображения JSON в удобном для чтения виде.
+Для работы скрипта необходимо ввести пусть до файла в формате JSON.
 
 # Quickstart
 
-[TODO]
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5.
 
-Example of script launch on Linux, Python 3.5:
+Пример запуска скрипта на Linux, Python 3.5:
 
 ```bash
-
 $ python pprint_json.py <path to file>
-# TODO add output example
-
 ```
+
+Пример работы скрипта:
+
+```bash
+$ python pprint_json.py my.json
+{
+    "пример": {
+        "Заголовок": "пример словаря",
+        "список": [
+            "элемент 1",
+            "элемент 2"
+        ]
+    }
+}
+```
+
 
 # Project Goals
 
--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,34 @@
 import json
+import argparse
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf8') as file:
+        return json.loads(file.read())
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='JSON file address')
+    parser.add_argument('filepath', nargs='?', default='my.json',
+                        help='An optional filepath')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    try:
+        filepath = create_parser().filepath
+        data = load_data(filepath)
+        pretty_print_json(data)
+    except FileNotFoundError:
+        exit('Файл не найден')
+    except json.decoder.JSONDecodeError:
+        exit('Файл не в формате JSON')
 
 
 if __name__ == '__main__':
-    pass
+    main()
